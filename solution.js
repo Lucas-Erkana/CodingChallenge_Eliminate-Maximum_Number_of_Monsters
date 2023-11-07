a80b9e9489ff5e9b0ac8b16a4063074160ceb525
Lucas David Erkana <lderkana21x@gmail.com>
2023-11-07 21:32:24 +0200

    Solution 7 November 2023

--- solution.js
+++ solution.js
@@ -1,9 +1,23 @@
-var functionname = function(parameter) {
-//Write code here
-  };
+var eliminateMaximum = function(dist, speed) {
+  let times = dist.map((d, i) => Math.ceil(d / speed[i])); // Calculate time for each monster
+  times.sort((a, b) => a - b);   // Sort the times
+  
+  let count = 0;
+  for(let i = 0; i < times.length; i++) {
+      if(times[i] > count) {
+          count++;  // If time is more than count, increment the count
+      } else {
+          break;   // If time is not more than count, break the loop
+      }
+  }
+  return count;
+}
 
 
-  let parameter = 'test data'
-console.log(functionname(parameter)) //expected result true
 
-module.exports = functionname;
+let parameter = 'test data'
+console.log(eliminateMaximum([1, 3, 4],[1, 1, 1])) //expected result 3
+console.log(eliminateMaximum([1, 1, 2, 3],[1, 1, 1, 1])) //expected result 1
+console.log(eliminateMaximum([3, 2, 4],[5, 3, 2])) //expected result 1
+
+module.exports = eliminateMaximum;
--- tests.js
+++ tests.js
@@ -1,6 +1,24 @@
-const funtionname = require('./solution');
+const eliminateMaximum = require('./solution');
 const assert = require('assert');
 
-describe('Tests', function () {
-//Write tests here
+describe('eliminateMaximum', function() {
+  it('should return 3 for [1, 3, 4] and [1, 1, 1]', function() {
+    assert.strictEqual(eliminateMaximum([1, 3, 4], [1, 1, 1]), 3);
+  });
+  
+  it('should return 1 for [1, 1, 2, 3] and [1, 1, 1, 1]', function() {
+    assert.strictEqual(eliminateMaximum([1, 1, 2, 3], [1, 1, 1, 1]), 1);
+  });
+  
+  it('should return 1 for [3, 2, 4] and [5, 3, 2]', function() {
+    assert.strictEqual(eliminateMaximum([3, 2, 4], [5, 3, 2]), 1);
+  });
+  
+  it('should return 0 for [] and []', function() {
+    assert.strictEqual(eliminateMaximum([], []), 0);
+  });
+  
+  it('should return 5 for [5, 5, 5] and [1, 1, 1]', function() {
+    assert.strictEqual(eliminateMaximum([5, 5, 5], [1, 1, 1]), 3);
+  });
 });
